a7f7258a6ffd3e1d6bd94c79c2bf92f56f5e3a82
yash-pokiya <yashpokiya@gmail.com>
2026-03-30 22:56:39 +0530

    fix photo just for test only 1 img

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
           {/* logo */}
           <div className="flex justify-center items-center gap-x-2">
             <img
-              src="/logo.png"
+              src="../public/logo.png"
               className="w-20 object-cover"
               alt="Logo"
             />
